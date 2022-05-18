970c854571774a5e011472199e2c920fc5400036
TinyPandar <85983534+TinyPandar@users.noreply.github.com>
2022-05-18 23:58:29 +0800

    Update main.cpp
    分离了函数部分

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fcntl.h>
 #include "io.h"
 #include "Chess.h"
+#include "functions.h"
 using namespace std;
 void printCheckerboard();
 void printChess(Chess& chess);
@@ -18,26 +19,4 @@ int main(){
     chessMap[5][5]= cg.generateChess("king",0);
     printChess(*chessMap[5][5]);
     cout<<chessMap[5][5]->getType();
-}
-
-
-
-void printChess(Chess& chess){
-    _setmode(_fileno(stdout), _O_U16TEXT);
-    if(chess.getSide() == 0){
-        if(chess.getType() == "king") wcout<<L"\x265A";
-        if(chess.getType() == "queen") wcout<<L"\x265B";
-        if(chess.getType() == "rook") wcout<<L"\x265C";
-        if(chess.getType() == "bishop") wcout<<L"\x265D";
-        if(chess.getType() == "knight") wcout<<L"\x265E";
-        if(chess.getType() == "pawn") wcout<<L"\x265F";
-    } else{
-        if(chess.getType() == "king") wcout<<L"\x2654";
-        if(chess.getType() == "queen") wcout<<L"\x2655";
-        if(chess.getType() == "rook") wcout<<L"\x2656";
-        if(chess.getType() == "bishop") wcout<<L"\x2657";
-        if(chess.getType() == "knight") wcout<<L"\x2658";
-        if(chess.getType() == "pawn") wcout<<L"\x2659";
-    }
-    _setmode(_fileno(stdout),O_TEXT);
 }
