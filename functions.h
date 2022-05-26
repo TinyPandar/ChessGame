8f369d6a652928e06956ed47343d99305472fdd0
Hunter-Ght <105800060+Hunter-Ght@users.noreply.github.com>
2022-05-26 22:03:22 +0800

    Update functions.h

--- functions.h
+++ functions.h
@@ -15,11 +15,11 @@ void hideCursor() {
     HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleCursorInfo(handle, &cursor);
 }
-void gotoxy(int x, int y) { //移动光标到（x，y）的函数
-    COORD c;  //COORD为windows API中定义的一种结构，表示一个字符再控制台屏幕上的坐标
+void gotoxy(int x, int y) { //Move the cursor to the function of (x,y)
+    COORD c;  
     c.X = x;
     c.Y = y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c); //此函数是API中定位光标位置的函数
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c); 
 }
 int setColor(int c) {//for easier to change color
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), c);      //for easier to change color
