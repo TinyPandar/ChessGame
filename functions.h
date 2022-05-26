9f0a9c23c08fbe6da3ffa5f259c6bd028533b702
TinyPandar <85983534+TinyPandar@users.noreply.github.com>
2022-05-26 19:06:03 +0800

    Update functions.h

--- functions.h
+++ functions.h
@@ -4,6 +4,61 @@
 
 
 using namespace std;
+
+void mainMove(int size) {
+    fflush(stdin);
+    gotoxy(36, 20);
+    t2 = time(NULL);
+    if (size == 0) {
+        cout << "Now is Turn to White!" << endl;
+    }
+    if (size == 1) {
+        cout << "Now is Turn to Black!" << endl;
+    }
+    cout << "Please select the pieces:(eg:a 2)";
+
+    scanf("%c %c", &select_1, &select_2);
+    fflush(stdin);
+    cout << "Please enter the position of the piece to go:(eg:a 3)";
+    scanf("%c %c", &select_3, &select_4);
+    fflush(stdin);
+    intSelect_1 = select_1 - 96, intSelect_2 = select_2 - 48, intSelect_3 = select_3 - 96, intSelect_4 =
+            select_4 - 48;
+    if (intSelect_1 <= 8 && intSelect_1 > 0 && intSelect_2 <= 8 && intSelect_2 > 0 && intSelect_3 <= 8 &&
+        intSelect_3 > 0 && intSelect_4 <= 8 && intSelect_4 > 0) {
+        if (chessMap[intSelect_1][intSelect_2] == NULL) {
+            reFlashChess();
+            gotoxy(45, 15);
+            cout << "ERROR INPUT,Please check again!" << endl;
+            return;
+        }
+        if (chessMap[intSelect_1][intSelect_2]->getSide() != size) {
+            reFlashChess();
+            gotoxy(45, 15);
+            cout << "ERROR INPUT,Please Input white chess!" << endl;
+            return;
+        }
+        if (chessMap[intSelect_1][intSelect_2]->move(intSelect_3, intSelect_4)) {
+            turn++;
+            changeSide();
+            reFlashChess();
+            return;
+        } else {
+            reFlashChess();
+            gotoxy(45, 15);
+
+            cout << "ERROR to move,please try again!" << endl;
+            return;
+        }
+    } else {
+        reFlashChess();
+        gotoxy(45, 15);
+        wcout << "ERROR INPUT,please check your Input again!" << endl;
+        return;
+    }
+
+}
+
 //the function hiding the cursor on the terminal,which is learnt from the internet
 void hideCursor() { 
     CONSOLE_CURSOR_INFO cursor;
